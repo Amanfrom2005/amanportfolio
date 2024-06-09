93256725adb02e066789611d10022a259a1ab2d0
Coding UP <masterfrom2006@gmail.com>
2024-06-09 10:27:47 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "YouTober", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        backDelay: 100,
+        loop: true
+    });
+});
